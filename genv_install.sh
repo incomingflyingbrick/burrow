ab71e8c9cee3f11b912417ce8fb7aa50197d1e5a
Ya Zhou Jiang <jiangyazhou@Yas-MacBook-Air.lan>
2024-05-18 14:06:13 +0800

    first commit

--- genv_install.sh
+++ genv_install.sh
@@ -1,3 +1,7 @@
 sudo pip install genv
 git clone https://github.com/run-ai/genv.git $HOME/genv
-python3 
+wget https://raw.githubusercontent.com/incomingflyingbrick/burrow/main/genv_run_time.py
+python3 genv_run_time.py
+systemctl restart docker
+cp -f $HOME/genv/genv-docker/genv-docker.sh /usr/local/bin/genv-docker
+genv-docker run --rm ubuntu env | grep GENV_
